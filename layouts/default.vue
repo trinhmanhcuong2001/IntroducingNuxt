3532c9db17345c10059924b3cf6667640039dacd
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-11-20 11:49:00 +0700

    b2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
+    <header>
+        <h1>Đây là header</h1>
+    </header>
     <div>
-        <p>Đây là layout</p>
         <slot />
     </div>
+    <footer>
+        <h1>Đây là footer</h1>
+    </footer>
 </template>
